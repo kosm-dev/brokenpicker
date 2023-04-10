5a37cc07d18d9380efd0fba13600aef971e28b17
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-04-10 11:06:20 +0300

    added dropdown

---
+++ app/js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+
+  
+// выпадающее меню
+  let intervalId;
+  const dropdownTogglers = document.querySelectorAll('.dropdown-toggle');
+  dropdownTogglers.forEach(toggler => {
+    toggler.addEventListener('click', e => {
+      const menu = e.currentTarget.dataset.path,
+        menuAttr = document.querySelector(`[data-target=${menu}]`),
+        dropdownMenus = document.querySelectorAll('.dropdown-menu');
+
+      dropdownMenus.forEach(dropdownMenu => {
+        if (!menuAttr.classList.contains('open')) {
+          dropdownMenu.classList.remove('dropdown-menu--active');
+          dropdownMenu.classList.remove('open');
+          menuAttr.classList.add('dropdown-menu--active');
+          intervalId = setTimeout(() => {
+            menuAttr.classList.add('open');
+          });
+        }
+        if (menuAttr.classList.contains('open')) {
+          clearTimeout(intervalId);
+          menuAttr.classList.remove('dropdown-menu--active');
+          intervalId = setTimeout(() => {
+            menuAttr.classList.remove('open');
+          });
+        }
+
+      });
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
